f2efcf19e85fd117c2e58e4144d62d481d8699be
masterhc <cris_ferreira_pires@hotmail.com>
2017-10-10 14:41:34 +0100

    Added error message
    added error message

--- commands/pesquisa/searchManga.js
+++ commands/pesquisa/searchManga.js
@@ -32,8 +32,18 @@ class Mangacommando extends commando.Command{
                
             };
             
-          mal.manga.searchManga(searchString).then(res =>mensagem(res, message)).catch(err => console.error(err));
-          
+          mal.manga.searchManga(searchString).then(res =>mensagem(res, message)).catch(err => falhou(err));
+          function falhou{
+          let embed2 = new Discord.Richembed();
+            embed2.setAuthor("Rem-chan", "https://i.imgur.com/g6FSNhL.png")
+            embed2.setColor(0xdb06db)
+            embed2.setDescription(err)
+            embed2.setFooter('Rem-chan em ', "https://i.imgur.com/g6FSNhL.png")
+            embed2.setTimestamp()
+            
+            message.channel.send({embed2})
+          }
+       
           function mensagem(res, message){
             const embed = new Discord.RichEmbed()
             
