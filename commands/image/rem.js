8db534d458a03d45b17eabe8b6ee6c2f3a105b53
masterhc <cris_ferreira_pires@hotmail.com>
2018-07-26 11:50:51 +0100

    fix?
    fix?

--- commands/image/rem.js
+++ commands/image/rem.js
@@ -20,7 +20,7 @@ class remcommando extends commando.Command{
            
             function message(path){
                 const embed = new discord.RichEmbed;
-               
+               message.channel.send(`https://cdn.ram.moe/'${path}`);
                 embed.setColor(0xb50000);
                 embed.setTitle('Imagem:')
                 embed.setDescription('[Original]'+`(${path})`);
