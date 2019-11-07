6df52cfa6c865376bb207b73f4d4a688247a9aba
masterhc <cris_ferreira_pires@hotmail.com>
2019-11-07 01:23:32 +0000

    coiso
    coiso

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ bot.on('ready', ()=>{
       
    
    console.log(`Rem is up and ready to serve on ${bot.guilds.size} servers, for ${bot.users.size} users.`);
-   console.log(`Rem is up and ready to serve on ${bot.guilds.id}`);
+   console.log(`Rem is up and ready to serve on ${bot.guilds.name}`);
 
     timeout()
 }});
