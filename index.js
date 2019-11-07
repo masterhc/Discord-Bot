b7c2b2c1453b1a8d0782f311460e29d6e53ffe56
masterhc <cris_ferreira_pires@hotmail.com>
2019-11-07 22:00:37 +0000

    coiso
    coiso

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ bot.on('ready', ()=>{
     //console.log(bot.channels.find('name', "👐bem-vindo").reactions.count)
     
     
-    console.log(`Rem is up and ready to serve on ${bot.channels.find('name', "👐bem-vindo").fetchMessage(445251380639170560).reactions.count}`);
+    //console.log(`Rem is up and ready to serve on ${bot.channels.find('name', "👐bem-vindo").fetchMessage(445251380639170560).reactions}`);
 
     timeout()
 }});
