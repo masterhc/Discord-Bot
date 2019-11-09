a3043fedf8c59e48a2965bc1504bc3f8fe1cbca0
masterhc <cris_ferreira_pires@hotmail.com>
2019-11-09 01:24:54 +0000

    Update index.js

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ bot.on('ready', ()=>{
                     .then(collected => {
                         const reaction = collected.first();
                         console.log("coiso")
-                        console.log(reaction.users.id)
+                        console.log(reaction.users)
                         
                     })
                     .catch(console.error()); 
