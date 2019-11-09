9bf999cbc00331908f50f7a28089f297b23b4038
masterhc <cris_ferreira_pires@hotmail.com>
2019-11-09 16:37:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ bot.on('ready', ()=>{
                         
                         reactor = reaction.users.first()
                         console.log("reactor:"+reactor.id)
-                        bot.guilds.find('id', '334456736633323520').fetchMember(reactor.id).name
+                        console.log(bot.guilds.find('id', '334456736633323520').fetchMember(reactor.id).name)
                             /*.then(console.log) 
                             .catch(console.error);*/
                     })
