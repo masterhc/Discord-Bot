3c9e48247a37531e8fd55313a6a94ee77c16a8bd
masterhc <cris_ferreira_pires@hotmail.com>
2020-03-28 13:10:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ bot.registry.registerCommandsIn(__dirname + "/commands");
 //Start Up Log
 bot.on('ready', ()=>{
     //log servers
-    console.log("guild name"+bot.guilds[0].name)
+    console.log("guild name"+bot.guilds[0].guild.name)
     let guildinfo = {
        "guilds":[
 
