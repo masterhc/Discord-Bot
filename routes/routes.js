278a7c76d4d8922be92c2a8fc249975eef0270c7
masterhc <cris_ferreira_pires@hotmail.com>
2021-10-23 21:22:39 +0100

    debug

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const home = require('../controllers/home.js');
 
 
-router.route('/').get(home.get());
+router.route('/').get(home.get);
 
 module.exports = router;
